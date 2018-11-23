6f88e236f73123739794c51c88bfdefbcc172762
YipengChen <chenypmail@163.com>
2018-11-23 18:51:20 +0800

    v0.2
    Test Train

---
+++ PrepareDatasets.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+datasets准备和beyond part方法一样，文件路径为
+/media/tj/ChenYP_Disk1/reid-datasets/beyond-part-models/market1501
+/media/tj/ChenYP_Disk1/reid-datasets/beyond-part-models/cuhk03
+/media/tj/ChenYP_Disk1/reid-datasets/beyond-part-models/duke
+
+
+还未部署market1501_cuhk03_duke   combined
