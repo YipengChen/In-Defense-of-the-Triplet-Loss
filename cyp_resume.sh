9b4b20b52dbb2a9a9b9b677926b0165b4039f5b5
YipengChen <chenypmail@163.com>
2018-11-24 19:28:47 +0800

    v0.3.4

---
+++ cyp_resume.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+python script/experiment/train.py \
+-d '(0,)' \
+--only_test false \
+--dataset market1501 \
+--last_conv_stride 2 \
+--normalize_feature false \
+--trainset_part trainval \
+--exp_dir ./exp_dir/20181123_market1501_trainval_stride2_default \
+--steps_per_log 10 \
+--epochs_per_val 5 \
+--resume True \
+--total_epochs 301
+
+python script/experiment/train.py \
+-d '(0,)' \
+--only_test false \
+--dataset cuhk03 \
+--last_conv_stride 2 \
+--normalize_feature false \
+--trainset_part trainval \
+--exp_dir ./exp_dir/20181123_cuhk03_trainval_stride2_default \
+--steps_per_log 10 \
+--epochs_per_val 5 \
+--resume True \
+--total_epochs 301
+
+python script/experiment/train.py \
+-d '(0,)' \
+--only_test false \
+--dataset duke \
+--last_conv_stride 2 \
+--normalize_feature false \
+--trainset_part trainval \
+--exp_dir ./exp_dir/20181123_duke_trainval_stride2_default \
+--steps_per_log 10 \
+--epochs_per_val 5 \
+--resume True \
+--total_epochs 301
