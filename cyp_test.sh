4ec991adec9217a1b45d18871246831afa015200
YipengChen <chenypmail@163.com>
2018-11-24 18:54:43 +0800

    v0.3
    Test test

---
+++ cyp_test.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+python script/experiment/train.py \
+-d '(0,)' \
+--only_test true \
+--dataset market1501 \
+--last_conv_stride 2 \
+--normalize_feature false \
+--exp_dir ./exp_dir/20181123_market1501_trainval_stride2_default \
+--model_weight_file ./exp_dir/20181123_market1501_trainval_stride2_default/ckpt.pth   
+
+python script/experiment/train.py \
+-d '(0,)' \
+--only_test true \
+--dataset cuhk03 \
+--last_conv_stride 2 \
+--normalize_feature false \
+--exp_dir ./exp_dir/20181123_cuhk03_trainval_stride2_default \
+--model_weight_file ./exp_dir/20181123_cuhk03_trainval_stride2_default/ckpt.pth 
+
+python script/experiment/train.py \
+-d '(0,)' \
+--only_test true \
+--dataset duke \
+--last_conv_stride 2 \
+--normalize_feature false \
+--exp_dir ./exp_dir/20181123_duke_trainval_stride2_default \
+--model_weight_file ./exp_dir/20181123_duke_trainval_stride2_default/ckpt.pth 
